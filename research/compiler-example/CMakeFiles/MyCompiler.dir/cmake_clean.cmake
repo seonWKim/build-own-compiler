ff412787af5160c508b3095efd475bef8e7fff4c
김선우 <seonwoo960000@toss.im>
2024-09-22 15:45:10 +0900

    until page 246

---
+++ research/compiler-example/CMakeFiles/MyCompiler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyCompiler.dir/src/lexer.cpp.o"
+  "CMakeFiles/MyCompiler.dir/src/lexer.cpp.o.d"
+  "MyCompiler"
+  "MyCompiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyCompiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
