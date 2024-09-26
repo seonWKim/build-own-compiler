b6cd5778557514504cacd25d741537eb3b1dbb32
김선우 <seonwoo960000@toss.im>
2024-09-27 08:02:31 +0900

    Done with page 273

--- personal/main.c
+++ personal/main.c
@@ -11,18 +11,27 @@ int main(int argc, const char* argv[]) {
     initChunk(&chunk);
 
 #define WRITE_CONSTANT(chunk, value, line1, line2) \
-    writeChunk(&chunk, OP_CONSTANT, line1);    \
-    writeChunk(&chunk, addConstant(&chunk, value), line2);
+    writeChunk(chunk, OP_CONSTANT, line1);    \
+    writeChunk(chunk, addConstant(chunk, value), line2);
 
-    WRITE_CONSTANT(chunk, 1, 123, 123);
-    WRITE_CONSTANT(chunk, 2, 124, 124);
-    writeChunk(&chunk, OP_NEGATE, 124);
+//    WRITE_CONSTANT(chunk, 1, 123, 123);
+//    WRITE_CONSTANT(chunk, 2, 124, 124);
+//    writeChunk(&chunk, OP_NEGATE, 124);
+//
+//    WRITE_CONSTANT(chunk, 3, 125, 125);
+//    WRITE_CONSTANT(chunk, 4, 126, 126);
+//    writeChunk(&chunk, OP_ADD, 127);
+//
+//    writeChunk(&chunk, OP_RETURN, 126);
+//    interpret(&chunk);
 
-    WRITE_CONSTANT(chunk, 3, 125, 125);
-    WRITE_CONSTANT(chunk, 4, 126, 126);
-    writeChunk(&chunk, OP_ADD, 127);
-
-    writeChunk(&chunk, OP_RETURN, 126);
+    WRITE_CONSTANT(&chunk, 1.3, 123, 123)
+    WRITE_CONSTANT(&chunk, 1.2, 123, 123)
+    writeChunk(&chunk, OP_ADD, 123);
+    WRITE_CONSTANT(&chunk, 5.6, 123, 123);
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
     interpret(&chunk);
 
     freeVM();
