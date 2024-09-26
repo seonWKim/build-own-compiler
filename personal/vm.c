4dca42230959cdebcba9f4bbb4ade9cd845926a9
김선우 <seonwoo960000@toss.im>
2024-09-27 08:26:43 +0900

    Completed steps before scanning

--- personal/vm.c
+++ personal/vm.c
@@ -82,13 +82,9 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk) {
-    // store the chunk being executed in the VM
-    vm.chunk = chunk;
-
-    // location of current instruction being executed
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
 
 void push(Value value) {
